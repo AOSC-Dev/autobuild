5a9b13d154734be1b45f688140061390863e101a
Arthur Wang <arthur200126@gmail.com>
2014-05-11 21:47:15 +0800

    Update genarch.sh

--- genarch.sh
+++ genarch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Autobuild arch generater.
-# -*- vim:fenc=utf-8:shiftwidth=2::softtabstop=2:autoindent
+# -*- vim:fenc=utf-8:shiftwidth=2::softtabstop=2
 if [ $1 == "" ] 
 then
   echo "Please specify your target architecture.\n  Usage: ./genarch.sh <arch> <extra dpkg flags>" >&2
   exit 1
 else
+  [ $1 == "amd64" ] && echo "Well, you are using amd64 and the original script is written for amd64..." && exit 0
   mkdir $1
   cp -R amd64/* $1
   cd $1
