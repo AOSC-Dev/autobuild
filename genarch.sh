1c205b45e50ce09b9aa44542c958829586343369
Arthur Wang <arthur200126@gmail.com>
2014-05-11 21:44:40 +0800

    Update genarch.sh

--- genarch.sh
+++ genarch.sh
@@ -3,15 +3,16 @@
 # -*- vim:fenc=utf-8:shiftwidth=2::softtabstop=2:autoindent
 if [ $1 == "" ] 
 then
-  echo "Please specify your target architecture.\n  Usage: ./genarch.sh <arch>" >&2
+  echo "Please specify your target architecture.\n  Usage: ./genarch.sh <arch> <extra dpkg flags>" >&2
   exit 1
 else
   mkdir $1
   cp -R amd64/* $1
   cd $1
   mv autobuild autobuild_old
-  sed "s/amd64/$1/" autobuild_old > autobuild
-  rm -f autobuild_old && chmod +x autobuild
+  sed "s/amd64/$1/" autobuild_old > autobuild_mid
+  sed "s/-Sextreme/$2/" autobuild_mid > autobuild
+  rm -f autobuild_* && chmod +x autobuild
   echo "Complete!"
  fi
 
