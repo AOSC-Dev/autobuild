15c1814c83862104af15f6bb4609bc51b22c2bae
Mingye Wang <arthur200126@gmail.com>
2016-01-21 19:52:42 -0500

    Update base_03cplink.sh

--- src/lib/plug/base_03cplink.sh
+++ src/lib/plug/base_03cplink.sh
@@ -1,3 +1,3 @@
 cp --version | grep -q GNU || return
 
-cp(){ command cp --reflink=auto; } # not doing -l; refer to scriptlets:cp-wrapper
+cp(){ command cp --reflink=auto "$@"; } # not doing -l; refer to scriptlets:cp-wrapper
